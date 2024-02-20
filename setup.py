c91e8dfb4de714b0d98db059a9d4550e2e91f135
Savino Piccolomo <savino.piccolomo@togetherhousing.co.uk>
2024-02-20 16:39:55 +0000

    Update version to 1.7

--- setup.py
+++ setup.py
@@ -12,7 +12,7 @@ setup(
     author = "Savino Piccolomo",
     author_email = "piccolomo@gmail.com",
     name = 'gioconda',
-    version='1.6.0',
+    version='1.7',
     description = 'handles tabular data',
     long_description = README,
     long_description_content_type = "text/markdown",  
